6870af062df1e3292ccfa01020fdda187970377d
druiz88 <druiza88@gmail.com>
2019-10-24 00:51:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/molitalia/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.molitalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bn;
+    EditText et;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        final EditText et = findViewById(R.id.editText);
+        final TextView tv = findViewById(R.id.textView);
+
+        readMolitaliaData();
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int len = molitaliaSamples.size();
+                for(int i=0; i<len; i++) {
+                    if (String.valueOf(molitaliaSamples.get(i).getCode()).equals(et.getText().toString())) {
+                        tv.setText(i);
+                    }
+                }
+            }
+        });
+    }
+
+    private List<MolitaliaSample> molitaliaSamples = new ArrayList<>();
+
+
+    private void readMolitaliaData() {
+        InputStream is = getResources().openRawResource(R.raw.data);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+
+        String line = "";
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(",");
+
+                MolitaliaSample sample = new MolitaliaSample();
+                sample.setCode(Integer.parseInt(tokens[0]));
+                sample.setText(tokens[1]);
+                sample.setPal(Integer.parseInt(tokens[2]));
+                molitaliaSamples.add(sample);
+
+                Log.d("MyActivity", "Just Created: " + sample);
+
+
+            }
+        } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/molitalia/MolitaliaSample.java
@@ -0,0 +1,42 @@
+package com.example.molitalia;
+
+import java.io.Serializable;
+
+class MolitaliaSample implements Serializable {
+    private int Code;
+    private String Text;
+    private int Pal;
+
+    public int getCode() {
+        return Code;
+    }
+
+    public void setCode(int code) {
+        Code = code;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public int getPal() {
+        return Pal;
+    }
+
+    public void setPal(int pal) {
+        Pal = pal;
+    }
+
+    @Override
+    public String toString() {
+        return "MolitaliaSample{" +
+                "Code=" + Code +
+                ", Text='" + Text + '\'' +
+                ", Pal=" + Pal +
+                '}';
+    }
+}
